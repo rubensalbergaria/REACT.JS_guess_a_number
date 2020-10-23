c9596f4cfb5af9a285ded3894ecc206422e2a1e7
Rubens Albergaria <rubensalbergaria@gmail.com>
2020-10-23 01:08:19 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [estado, setEstado] = useState("ENTRADA");
+
+  const [palpite, setPalpite] = useState(150);
+  const [numPalpites, setNumPalpites] = useState(1);
+
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(300);
+
+  const iniciarJogo = () => {
+    setEstado("RODANDO");
+    setMin(0);
+    setMax(300);
+    setNumPalpites(1);
+    setPalpite(150);
+  };
+  if (estado === "ENTRADA") {
+    return (
+      <button onClick={iniciarJogo}>
+        Mentalize um número de 0 a 300 e clique aqui!
+      </button>
+    );
+  }
+
+  const menor = () => {
+    setNumPalpites(numPalpites + 1);
+    setMax(palpite);
+    const proxPalpite = parseInt((palpite - min) / 2) + min;
+    setPalpite(proxPalpite);
+  };
+
+  const maior = () => {
+    setNumPalpites(numPalpites + 1);
+    setMin(palpite);
+    const proxPalpite = parseInt((max - palpite) / 2) + palpite;
+    setPalpite(proxPalpite);
+  };
+
+  const acertou = () => {
+    setEstado("FIM");
+  };
+  if (estado === "FIM") {
+    return (
+      <div>
+        <p>
+          Acertei o seu número {palpite} com {numPalpites} chute(s)!
+        </p>
+        <button onClick={iniciarJogo}>iniciar novamente!</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <p>O seu número é {palpite}?</p>
+      <button onClick={menor}>Menor!</button>
+      <button onClick={acertou}>Acertou!</button>
+      <button onClick={maior}>Maior!</button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
